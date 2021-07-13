4bd140db250bbc9d9abb3b61e3635b198421c92b
Amna Syeda <amna.syeda98@gmail.com>
2021-07-12 21:36:17 -0400

    dependencies

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 const db = require('./db/connection');
 const { startPrompt } = require('./lib/prompts');
 
-const express = require('express');
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-
 db.connect(err => {
   if(err) throw err;
   console.log(`
