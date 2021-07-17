8eaa7f7a0d54e1f2981b964cb027460f13e412cb
Amna Syeda <amna.syeda98@gmail.com>
2021-07-16 21:47:19 -0400

    removed password in connection.js

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,8 @@ const db = mysql.createConnection(
   {
     host : 'localhost',
     user : 'root',
-    password: '6026121603pink',
-    database: "employee"
+    password: '',
+    database: 'employee'
   }
 )
 
